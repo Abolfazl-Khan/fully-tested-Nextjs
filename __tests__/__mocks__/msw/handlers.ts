2ef5cb1730f548b3d369de01c8c33a89285d71b4
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-03-02 21:41:16 +0330

    updated handlers to handle shows api

--- __tests__/__mocks__/msw/handlers.ts
+++ __tests__/__mocks__/msw/handlers.ts
@@ -1,3 +1,14 @@
 import { rest } from 'msw';
 import { readFakeData } from '../fakeData';
-export const handlers = [];
+
+export const handlers = [
+  rest.get('http://localhost:3000/api/shows/:showId', async (req, res, ctx) => {
+    const { fakeShows } = await readFakeData();
+    const { showId } = req.params;
+    return res(
+      ctx.json({
+        show: fakeShows[+showId],
+      })
+    );
+  }),
+];
