701a4fd5f0cb613c346fbbcc5a04c08551e0f0a7
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-03-04 22:09:00 +0330

    updated handlers to add a method for reservations

--- __tests__/__mocks__/msw/handlers.ts
+++ __tests__/__mocks__/msw/handlers.ts
@@ -1,5 +1,6 @@
 import { rest } from 'msw';
 import { readFakeData } from '../fakeData';
+import { fakeUserReservations } from '../fakeData/userReservations';
 
 export const handlers = [
   rest.get('http://localhost:3000/api/shows/:showId', async (req, res, ctx) => {
@@ -11,4 +12,13 @@ export const handlers = [
       })
     );
   }),
+  rest.get(
+    'http://localhost:3000/api/users/:userId/reservations',
+    (req, res, ctx) => {
+      const { userId } = req.params;
+      const userReservations = +userId === 1 ? fakeUserReservations : [];
+
+      return res(ctx.json({ userReservations }));
+    }
+  ),
 ];
