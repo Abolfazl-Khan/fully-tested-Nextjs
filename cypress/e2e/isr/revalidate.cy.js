ce9178f15c2f3155b2d61a11f5f3298ca20a9421
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-03-30 15:05:25 +0330

    updated revalidate.cy to add another test for shows page

--- cypress/e2e/isr/revalidate.cy.js
+++ cypress/e2e/isr/revalidate.cy.js
@@ -1,4 +1,5 @@
 import { generateNewBand } from '../../../__tests__/__mocks__/fakeData/newBand';
+import { generateNewShow } from '../../../__tests__/__mocks__/fakeData/newShow';
 import { generateRandomId } from '../../../lib/features/reservations/utils';
 
 it('should load refreshed page from cache after new band is added', () => {
@@ -11,14 +12,31 @@ it('should load refreshed page from cache after new band is added', () => {
   const newBand = generateNewBand(newBandId);
   const secret = Cypress.env('REVALIDATION_SECRET');
 
-  cy.request('POST', `/api/bands?secret=${secret}`, { newBand: newBand }).then(
-    (response) => {
-      expect(response.body.revalidated).to.equal(true);
-    }
-  );
+  cy.request('POST', `/api/bands?secret=${secret}`, { newBand }).then((res) => {
+    expect(res.body.revalidated).to.equal(true);
+  });
 
   cy.reload();
   cy.findByRole('heading', { name: /avalanche of cheese/i }).should('exist');
 
+  cy.resetDbAndIsrCache();
+});
+
+it('should load refreshed page from cache after new show is added', () => {
+  cy.task('db:reset').visit('/shows');
+  cy.findByRole('heading', { name: /avalanche of cheese/i }).should(
+    'not.exist'
+  );
+  const showId = generateRandomId();
+  const newShow = generateNewShow(showId);
+
+  const secret = Cypress.env('REVALIDATION_SECRET');
+
+  cy.request('POST', `/api/shows?secret=${secret}`, { newShow }).then((res) =>
+    expect(res.body.revalidated).to.equal(true)
+  );
+
+  cy.reload();
+  cy.findByRole('heading', { name: /avalanche of cheese/i }).should('exist');
   cy.resetDbAndIsrCache();
 });
