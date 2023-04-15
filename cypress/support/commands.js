d50888f5a440bc912d8cf3222d3bafc4e4ea2589
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-04-15 23:21:36 +0330

    updated commands to add new one for singIn

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,3 +5,21 @@ Cypress.Commands.add('resetDbAndIsrCache', () => {
   const secret = Cypress.env('REVALIDATION_SECRET');
   cy.request('GET', `/api/revalidate?secret=${secret}`);
 });
+
+Cypress.Commands.add('signIn', (email, password) => {
+  // it would be better to use api here instead of signing in through UI
+  cy.visit('/auth/signin');
+
+  cy.findByLabelText(/email address/i)
+    .clear()
+    .type(email);
+  cy.findByLabelText(/password/i)
+    .clear()
+    .type(password);
+
+  cy.findByRole('main').within(() => {
+    cy.findByRole('button', { name: /sign in/i }).click();
+  });
+
+  cy.findByRole('heading', { name: /welcome/i }).should('exist');
+});
