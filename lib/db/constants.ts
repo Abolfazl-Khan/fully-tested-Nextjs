5970a6389f9d9521afa5935df026884e0df27c40
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-04-18 23:01:26 +0330

    updated [...nextauth] to use the new getSignedInUser util to fix login bug in production

--- lib/db/constants.ts
+++ lib/db/constants.ts
@@ -2,7 +2,7 @@ export const venueCapacity = 400;
 
 export const getDbPath = (): string => {
   if (!process.env.DB_PATH) {
-    throw new Error('Missing process.env.DB_PATH');
+    throw new Error("Missing process.env.DB_PATH");
   }
 
   return process.env.DB_PATH;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,35 +1,33 @@
 /* eslint-disable no-param-reassign */
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-
-import { axiosInstance } from "@/lib/axios/axiosInstance";
-import { routes } from "@/lib/axios/routes";
 import type { User } from "@/lib/features/users/types";
+import { getSignedInUser } from "@/lib/features/users/utils";
 
 export default NextAuth({
   providers: [
     // reference: https://next-auth.js.org/configuration/providers/credentials#how-to
     CredentialsProvider({
-      id: "credentials",
+      // id: 'credentials',
       // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: "credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, email, password, 2FA token, etc.
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: { label: "email", type: "text", placeholder: "test" },
         password: { label: "Password", type: "password", placeholder: "test" },
       },
-      async authorize(credentials) {
-        const { data: user } = await axiosInstance({
-          url: `/api/${routes.users}`,
-          method: "POST",
-          data: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        // If no error and we have user data, return it
-        // Return null if user data could not be retrieved
+
+      // async authorize(credentials) {
+      async authorize(credentials): Promise<{ user: User } | null> {
+        const user = await getSignedInUser(
+          credentials?.email ?? "",
+          credentials?.password ?? ""
+        );
+
+        if (!user) return null;
         return user ?? null;
       },
     }),
@@ -48,6 +46,7 @@ export default NextAuth({
     async jwt({ token, user }) {
       // reference: https://next-auth.js.org/configuration/callbacks#jwt-callback
       // Persist the JWT token to the token right after signin
+      // console.log(10, token, user);
       if (user) {
         token.user = user.user;
       }
@@ -57,11 +56,17 @@ export default NextAuth({
     async session({ session, token, user }) {
       // reference: https://next-auth.js.org/configuration/callbacks#session-callback
       // Send properties to the client, like an access_token from a provider
+      // console.log(0);
+
+      // console.log(1, session, token);
+      // console.log(1, session, token, user);
 
       const tokenUser = token.user as User;
 
       session.token = tokenUser.token;
+
       session.user = tokenUser;
+
       return session;
     },
   },
